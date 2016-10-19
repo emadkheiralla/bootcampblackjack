15980a4e3dab95bb7289411e2fbd4c3a85868466
Daniel Lopez <Daniel Lopez>
2016-10-18 20:01:16 -0400

    adding heroku kinda

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 ﻿require('rootpath')();
 var express = require('express');
 var mongo = require('mongojs');
+var mongoose = require('mongoose');
+//var db = mongo(process.env.MONGODB_URI, ['demoCollection']);
 var app = express();
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
 var config = require('config.json');
 
+var databaseUri = 'mongodb://heroku_svpplccz:ekt9qdko4jk87ij6mgn9pkbc99@ds061506.mlab.com:61506/heroku_svpplccz';
+
+if (process.env.MONGODB_URI) {
+
+mongoose.connect(process.env.MONGODB_URI);
 
 
+
+} else {
+	mongoose.connect(databaseUri);
+}
+
+var db = mongoose.connection;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: false }));
