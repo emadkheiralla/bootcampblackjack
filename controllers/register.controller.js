e7a53d145e2c1efc4619437df2fe0456699a1386
Emad Kheiralla <emadkheiralla@hotmail.com>
2016-10-26 17:26:43 -0400

    Now Finally fixed Heroku issue for real for real

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res) {
     // register using api to maintain clean separation between layers
     request.post({
-        url: 'http://127.0.0.1:' + process.env.PORT + 'users/register',
+        url: 'http://127.0.0.1:' + process.env.PORT + '/api/users/register',
         form: req.body,
         json: true
     }, function (error, response, body) {
