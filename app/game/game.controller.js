91091c5a4099148f341ea222d850442a9f97fc74
Emad Kheiralla <emadkheiralla@hotmail.com>
2016-10-26 22:46:39 -0400

    more styling

--- app/game/game.controller.js
+++ app/game/game.controller.js
@@ -471,8 +471,8 @@
 		var	rank  = card.getRank(),
 				suit  = card.getSuit(),
 				color = 'red',
-				posx  = 100vmax,
-				posy  = 25vmax,
+				posx  = 50%,
+				posy  = 25%,
 				speed = 200,
 				cards = ele + ' .card-' + i;
 
@@ -495,7 +495,7 @@
 			);
 
 			if(ele === '#phand') {
-				posy  = 75vmax;
+				posy  = 75%;
 				speed = 500;
 				$(ele + ' div.card-' + i).attr('id', 'pcard-' + i);
 
